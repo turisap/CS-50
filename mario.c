771cfae4686675128058b9245dc45a226cb8a647
turisap <kirillshakirov57@gmail.com>
2020-02-04 21:55:04 +0300

    mario

--- mario.c
+++ mario.c
@@ -7,15 +7,36 @@ int main(void)
 {
     int height;
 
-    do {
+    do
+    {
         height = get_int("I need heights of the pyramid \n");
-    } while(height > 8 || height < 1);
+    } while (height > 8 || height < 1);
 
-    for(int i = 1; i <= height; i++) {
-        for(int j = height * 2 + 2; j > 0; j--) {
-            printf(" ");
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = 1; j <= height * 2 + 2; j++)
+        {
+
+            if (j <= height - i)
+            {
+                printf(" ");
+            }
+
+            if (j > height - i && j <= height)
+            {
+                printf("#");
+            }
+
+            if (j > height && j <= height + 2)
+            {
+                printf(" ");
+            }
+
+            if (j >= height + 2 && j < height + 2 + i)
+            {
+                printf("#");
+            }
         }
         printf("\n");
     }
-
 }
