b16a3929a3f84f9a1877b889885457d6468532bc
turisap <kirillshakirov57@gmail.com>
2020-02-16 21:18:07 +0300

    greyscale filter

--- filter.c
+++ filter.c
@@ -1,11 +1,30 @@
 #include "helpers.h"
+#include <stdio.h>
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    printf("%i", height);
-    printf("%i", width);
-    printf("%d", image);
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int green = image[i][j].rgbtGreen;
+            int red = image[i][j].rgbtRed;
+            int blue = image[i][j].rgbtBlue;
+
+            int average = floor((red + green + blue) / 3);
+
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtRed = average;
+            image[i][j].rgbtBlue = average;
+
+            printf("%i\n", image[i][j].rgbtGreen);
+            printf("%i\n", image[i][j].rgbtBlue);
+            printf("%i\n", image[i][j].rgbtRed);
+            printf("===============");
+        }
+    }
     return;
 }
 
