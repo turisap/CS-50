87be1f6817bda33e51241f839fd10cd0cd92a8a8
turisap <kirillshakirov57@gmail.com>
2020-02-16 23:24:04 +0300

    blur filter

--- filter.c
+++ filter.c
@@ -67,9 +67,48 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     }
     return;
 }
-
 // Blur image
+// THIS IS A VERY SILLY IMPLEMENTAIONT DUE TO 4 NESTED FOR LOOPS :(
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int sum_red = 0;
+            int sum_green = 0;
+            int sum_blue = 0;
+            int cell_count = 0;
+
+            int row_range_start = i - 1 >= 0 ? i - 1 : 0;
+            int row_range_end = i + 1 < height ? i + 1 : height - 1;
+
+            int column_range_start = j - 1 >= 0 ? j - 1 : 0;
+            int column_range_end = j + 1 < width ? j + 1 : width - 1;
+
+            for (int el = column_range_start; el <= column_range_end; el++)
+            {
+
+                for (int sub_row = row_range_start; sub_row <= row_range_end; sub_row++)
+                {
+
+                    sum_red += image[sub_row][el].rgbtRed;
+                    sum_green += image[sub_row][el].rgbtGreen;
+                    sum_blue += image[sub_row][el].rgbtBlue;
+
+                    cell_count++;
+                }
+            }
+
+            int average_red = sum_red / cell_count;
+            int average_green = sum_green / cell_count;
+            int average_blue = sum_blue / cell_count;
+
+            image[i][j].rgbtRed = average_red;
+            image[i][j].rgbtGreen = average_green;
+            image[i][j].rgbtBlue = average_blue;
+        }
+    }
+
     return;
 }
