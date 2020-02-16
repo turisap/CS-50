75960a6fb216c31e0bcd5b81c348f10e3fa41035
turisap <kirillshakirov57@gmail.com>
2020-02-16 21:33:27 +0300

    sepia effect

--- filter.c
+++ filter.c
@@ -18,11 +18,6 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             image[i][j].rgbtGreen = average;
             image[i][j].rgbtRed = average;
             image[i][j].rgbtBlue = average;
-
-            printf("%i\n", image[i][j].rgbtGreen);
-            printf("%i\n", image[i][j].rgbtBlue);
-            printf("%i\n", image[i][j].rgbtRed);
-            printf("===============");
         }
     }
     return;
@@ -31,6 +26,27 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int red = image[i][j].rgbtRed;
+            int green = image[i][j].rgbtGreen;
+            int blue = image[i][j].rgbtBlue;
+
+            int sepiaRed = floor(0.393 * red + 0.769 * green + 0.189 * blue);
+            int sepiaGreen = floor(0.349 * red + 0.686 * green + 0.168 * blue);
+            int sepiaBlue = floor(0.272 * red + 0.534 * green + 0.131 * blue);
+
+            sepiaRed = sepiaRed > 255 ? 255 : sepiaRed;
+            sepiaGreen = sepiaGreen > 255 ? 255 : sepiaGreen;
+            sepiaBlue = sepiaBlue > 255 ? 255 : sepiaBlue;
+
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
+        }
+    }
     return;
 }
 
